10d598838bf382dcc785f127654b351324eb9fde
SwathiMKKulal <swathimkkulal@gmail.com>
2020-03-10 11:31:21 +0530

    add user data to index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def create
-		debugger
+		# debugger
 		@article = Article.new(article_params)
 		@article.user = User.first
 		if @article.save
