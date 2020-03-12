806ed2c91e7898688ac0425ea9bf98fc27d89aaf
SwathiMKKulal <swathimkkulal@gmail.com>
2020-03-12 15:55:19 +0530

    edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,18 @@ def create
 		render 'new'
 	end
 end
+def edit
+	@category = Category.find(params[:id])
+end
+def update
+	@category = Category.find(params[:id])
+	if @category.update(category_params)
+		flash[:success] = "name was successfuly updated"
+		redirect_to category_path(@category)
+	else
+	render 'edit'
+	end
+end
 def show
 	@category = Category.find(params[:id])
 	@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
