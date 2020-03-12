acefa98523b6f602ca5a2f57b777593d09fbc4ed
SwathiMKKulal <swathimkkulal@gmail.com>
2020-03-12 12:11:10 +0530

    categories

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,18 @@
+require 'rake/testtask'
+class CategoriesControllerTest < ActionController::TestCase
+	def setup
+		@category = Category.create(name: "sports")
+	end
+	test "should get categories index" do
+		get :index
+		assert_response :success
+	end
+	test "should get new" do
+		get :new
+		assert_response :success
+	end
+	test "should get show" do
+		get(:show, {'id' => @category.id})
+		assert_response :success
+	end
+end
