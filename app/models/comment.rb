cdcd9a81ebb17dd5a13d6d96f1a7134f93b0cf34
SwathiMKKulal <swathimkkulal@gmail.com>
2020-03-19 11:28:02 +0530

    error messages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :comment, presence: { message: "field cant be blank"}
+  validates :comment, presence: { message: "field cant be blank! please enter your comment"}
   validates :comment, uniqueness: { case_sensitive: false }
   belongs_to :article
   belongs_to :user
